b17d2e8c0da73f38c86dead5d11b7e54a58ccdf7
Mattis Erkensten <m.erkensten@icloud.com>
2022-05-13 19:57:56 +0200

    Lagt till så att produktkorten länkar till rätt sida och att hela kortet är klickbart

--- src/components/products/ProductsCard/ProductsCard.jsx
+++ src/components/products/ProductsCard/ProductsCard.jsx
@@ -1,5 +1,6 @@
 // imports
-import React from 'react';
+import * as React from 'react';
+import Link from 'next/link';
 
 // components
 
@@ -10,15 +11,21 @@ export const ProductsCard = ({ product }) => {
   return (
     <>
       <div className={styles.card_wrapper}>
-        <div className={styles.card_img_wrapper}>
-          <img src={`/images/${product.img}`} alt={product.title} />
-        </div>
-        <div className={styles.card_content_wrapper}>
-          <h2 className={styles.card_title}>{product.title}</h2>
-          <p className={styles.card_description}>{product.description}</p>
-          <p className={styles.card_material}>Material: {product.material}</p>
-          <h3 className={styles.card_price}>{product.price} $</h3>
-        </div>
+        <Link href={`/shop/${product.category}/${product.slug}`}>
+          <a>
+            <div className={styles.card_img_wrapper}>
+              <img src={`/images/${product.img}`} alt={product.title} />
+            </div>
+            <div className={styles.card_content_wrapper}>
+              <h2 className={styles.card_title}>{product.title}</h2>
+              <p className={styles.card_description}>{product.description}</p>
+              <p className={styles.card_material}>
+                Material: {product.material}
+              </p>
+              <h3 className={styles.card_price}>{product.price} $</h3>
+            </div>
+          </a>
+        </Link>
       </div>
     </>
   );
