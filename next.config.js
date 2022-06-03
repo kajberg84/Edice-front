b5224e6bfe234253e153f7723e2a01847d83ecbf
Kaj <kb223aw@student.lnu.se>
2022-06-03 12:59:30 +0200

    config fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://example.com'],
+}
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
