358cd2d058f6e8eda6ef82f45704c4ea2040a549
Mattis Erkensten <m.erkensten@icloud.com>
2022-06-07 18:31:10 +0200

    inloggnings formuläret påbörjat, dock ej fungarande

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,25 +1,28 @@
 // Imports
-import { useContext } from "react";
-import { useRouter } from "next/router";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import React from 'react';
+import { useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 // context
-import { UserContext } from "../context/UserContext";
+import { UserContext } from '../context/UserContext';
 
 // components
-import { Seo } from "../components/seo/Seo";
-import { Wrapper } from "../components/layout/wrapper/Wrapper";
-import { UnAuthWrapper } from "../components/layout/wrapper/UnAuthWrapper";
+import { Seo } from '../components/seo/Seo';
+import { Wrapper } from '../components/layout/wrapper/Wrapper';
+import { UnAuthWrapper } from '../components/layout/wrapper/UnAuthWrapper';
 
 // helpers
-import { RoutingPath } from "../helpers/RoutingPath";
-import { getUser } from "../api/mockusers";
-import { setLocalStorage } from "../utils/localStorageHandler";
+import { RoutingPath } from '../helpers/RoutingPath';
+import { getUser } from '../api/mockusers';
+import { setLocalStorage } from '../utils/localStorageHandler';
 
 // styles
-import styles from "../styles/pages/Login.module.scss";
+import styles from '../styles/pages/Login.module.scss';
 
 // Schema for formvalidating
 const loginSchema = yup
@@ -32,30 +35,80 @@ const loginSchema = yup
 export default function Login() {
   const router = useRouter();
   const { setUser } = useContext(UserContext);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  // console.log(email, password);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    mode: "onBlur",
+    mode: 'onBlur',
     resolver: yupResolver(loginSchema),
   });
+
+  async function postAxios(userData, URL) {
+    const sendUrl = URL;
+    try {
+      const response = await axios({
+        url: sendUrl,
+        method: 'POST',
+        data: {
+          email: userData.email,
+          password: userData.password,
+        },
+      });
+      const { data } = response;
+      return data;
+    } catch (error) {
+      console.log('Error i post axios', error);
+    }
+  }
+
+  // Handling response from Login
+  function handleRespLogin(tokens) {
+    const parsedJwt = jwt_decode(tokens.access_token);
+    setUser(parsedJwt);
+    router.push('/');
+  }
+
+  // handle login
+  async function handleLogin(userData) {
+    const responseLogin = await postAxios(
+      { userData },
+      `https://edice-back.herokuapp.com/login`
+    );
+    if (responseLogin) {
+      handleRespLogin(responseLogin);
+    } else {
+      console.log('login response false, fel lösen / user');
+      //visa error
+      setShowErrorStatus(true);
+    }
+  }
 
   // On submit
   const onSubmit = async (data) => {
     try {
-      const responseUser = await getUser(data.email, data.password);
+      // const responseUser = await getUser(data.email, data.password);
+      user = {
+        email: email,
+        password: password,
+      };
+      console.log('user:', user);
+      handleLogin(user);
 
       if (!responseUser) {
-        alert("Wrong email/ password or no user found");
+        alert('Wrong email/ password or no user found');
         return;
       }
 
-      setLocalStorage("edice-user", responseUser);
+      setLocalStorage('edice-user', responseUser);
       setUser(responseUser);
     } catch (error) {
-      console.log("Login: ", error);
+      console.log('Login: ', error);
     }
 
     router.push(`/${RoutingPath.Account}`);
@@ -80,14 +133,16 @@ export default function Login() {
                   className={styles.login_form}
                 >
                   <input
-                    {...register("email")}
+                    {...register('email')}
                     placeholder="Type in your email"
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                   <p>{errors.email?.message}</p>
                   <input
-                    {...register("password")}
+                    {...register('password')}
                     placeholder="Type in your password"
                     type="password"
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                   <button
                     type="submit"
